91592c3441aed2cc9ffec4c7de7ffbe4884a0a25
Amutha varsni R <amuthavarsni.cs18@bitsathy.ac.in>
2022-06-27 14:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
   
 @app.route('/')
 def hello():
-    return "Hello from jenkins"
+    return "Hello from App Engine"
   
   
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 5000, debug = True) 
+    app.run(host ='0.0.0.0', port = 8080, debug = True) 
